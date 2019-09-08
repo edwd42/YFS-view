3ea95d3d386450e26c69470167f42ab7d287a9de
edwd42 <edwd42@yahoo.com>
2019-09-07 23:05:22 -0400

    edit homepage to reflect CareerDevs Capstone Yahoo Scraper Project

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,36 @@
 import React from "react";
 
 import logo from "../assets/logo.svg";
+import yahoo_finance from "../assets/yahoo_finance.png";
 
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <h1 className="mb-4">CareerDevs Capstone Yahoo Scraper Project - MVP</h1>
 
     <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      <div className="flex-container">
+        <div id="article">
+          <article>
+            <p>
+              <em>
+                A CareerDevs web scraper project demonstrating basic principles
+                of web scraping.&nbsp;&nbsp;
+              </em>
+              With a made up Yahoo Finance portfolio of 10 stocks, I implement
+              several web technologies, frameworks and libraries including Java,
+              Selenium and Spring Boot to scrape data from Yahoo Finance and
+              display it to the user. Design Patterns guided my development
+              process, including Singleton Pattern, Factory Pattern and State
+              Pattern adapted from the book Design Patterns - Elements of
+              Reusable Object-oriented Software, by Gang of Four, 1994.
+            </p>
+          </article>
+        </div>
+        <div id="img">
+          <img src={yahoo_finance} alt="Yahoo Finance Watchlist Page" />
+        </div>
+      </div>
     </p>
   </div>
 );
