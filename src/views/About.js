0c606654b58a7a5c891b138cc9b14fce5a160191
edwd42 <edwd42@yahoo.com>
2019-09-09 11:49:32 -0400

    edit About page

--- src/views/About.js
+++ src/views/About.js
@@ -19,37 +19,55 @@ function about() {
         <article>
           <h1>CareerDevs Capstone Scraper Project</h1>
           <p>
-            <span style={spanStyle}>Main Task </span>Build a Java scraper that
-            will pull stock data from your personal Yahoo finance account. This
-            application should store each scrape in a database so as to be able
-            to retain a history of past scraped data. This scraper will include
-            a web-based user facing interface, which pulls data from a RESTAPI
-            as JSON data, where the user will be able to view the historic data,
-            request a new scrape, and see the most recent scraped data as well.
+            <span style={spanStyle}>Main Task </span>
+            <br />
+            Build a Java scraper that will pull stock data from your personal
+            Yahoo finance account. This application should store each scrape in
+            a database so as to be able to retain a history of past scraped
+            data. This scraper will include a web-based user facing interface,
+            which pulls data from a RESTAPI as JSON data, where the user will be
+            able to view the historic data, request a new scrape, and see the
+            most recent scraped data as well.
           </p>
-
           <p>
             Following the MVC pattern (Model-View-Controller) I built this app
             using Java and Spring Boot for the controller, ReactJS for the view
-            and MongoDB on the back-end. The diagram below depicts controller
-            and database layers.
+            and MongoDB on the back-end.
           </p>
           <p>
-            I chose to write the view in ReactJS because it is fast, lightweight
-            and portable.
+            I wrote the view in ReactJS because it is fast, lightweight and
+            portable. I also like JavaScript and want to learn React. Also, when
+            building an app with the SpringMVC pattern, the view in JSP is
+            tightly coupled to the controller, which is bad programming design.
+            With React, code modules can be modified without affecting other
+            code.
           </p>
         </article>
       </div>
-      <div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img src={reactjs_logo} alt="reactjs logo"></img>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img src={java_logo} alt="java logo" />
+          </div>
+          <div className="col">
+            <img src={spring_logo} alt="spring logo"></img>
+          </div>
+          <div className="col">
+            <img
+              src={mongodb_logo}
+              alt="mongodb logo"
+              style={{ height: "50%", width: "75%" }}
+            ></img>
+          </div>
+        </div>
+        <hr />
+        <p>The diagram below depicts controller and database layers.</p>
         <img src={SpringBootStack} alt="Spring Boot Stack"></img>
-        <img src={reactjs_logo} alt="reactjs logo"></img>
-        <img src={java_logo} alt="java logo" />
-        <img src={spring_logo} alt="spring logo"></img>
-        <img
-          src={mongodb_logo}
-          alt="mongodb logo"
-          style={{ height: "50%", width: "35%" }}
-        ></img>
       </div>
     </>
   );
