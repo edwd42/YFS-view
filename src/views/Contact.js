e0ceea2b9dedcd39ce0330a8920723cf67c5b188
edwd42 <edwd42@yahoo.com>
2019-09-11 12:46:01 -0400

    add css to Contact

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,31 +1,73 @@
+// make a box with red at the top and white at bottom with 1px border
 import React from "react";
+// import "../assets/contact.css";
+
+const top = {
+  width: "398px",
+  height: "80px",
+  backgroundColor: "red",
+  padding: "10px 10px 10px 10px",
+  fontSize: "24px"
+};
+
+const bottom = {
+  width: "398px",
+  height: "160px",
+  backgroundColor: "white",
+  padding: "10px"
+};
+
+const container = {
+  border: "1px solid #000",
+  width: "400px"
+};
 
 const Contact = () => (
-  <>
-    <div>
-      <p>Please excuse my appearance. I am under construction. :)</p>
+  <div style={container}>
+    <div style={top}>
+      <span>Edward Dupre</span>
+      <br />
+      <span>Software Developer</span>
       <br />
     </div>
-    <div>
-      Edward Dupre
+    <div style={bottom}>
+      <span>Cell Phone: 401-497-6933</span>
       <br />
-      Software Developer
+      <span>
+        Email:
+        <a href="edward.dupre@edwd42.com">edward.dupre@edwd42.com</a>
+      </span>
       <br />
+      <span>
+        Twitter:
+        <a href="@edwd42" target="_blank" alt="twitter">
+          @edwd42
+        </a>
+      </span>
       <br />
-      Cell Phone: 401-497-6933
+      <span>
+        GitHub:
+        <a href="https://github.com/edwd42" target="_blank" alt="GitHub">
+          https://github.com/edwd42
+        </a>
+      </span>
       <br />
-      Email: Edward.Dupre@edwd42.com
+      <span>
+        LinkedIn:
+        <a href="www.linkedin.com/edwd42" target="_blank" alt="LinkedIn">
+          www.linkedin.com/edwd42
+        </a>
+      </span>
       <br />
-      Twitter: @edwd42
-      <br />
-      GitHub: https://github.com/edwd42
-      <br />
-      LinkedIn: www.linkedin.com/edwd42
-      <br />
-      Web: https://edwd42.com
+      <span>
+        Web:
+        <a href="https://edwd42.com" target="_blank" alt="web site">
+          https://edwd42.com
+        </a>
+      </span>
       <br />
     </div>
-  </>
+  </div>
 );
 
 export default Contact;
